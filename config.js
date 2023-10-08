544827d9e4464f35d93a2ec71dd40980d4d43d36
kookie2805 <142863415+kookie2805@users.noreply.github.com>
2023-10-08 19:43:12 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+var config = {
+
+    texts: [
+        "Hello Keisya",      
+        "Happy Level Up Day!!",
+        "May you always be given health, and happiness",
+        "I hope all your birthday wishes and dreams come true.",
+        "🤍🤍",
+        
+    ],
+    
+    imgs: {
+        "心爱的小可爱": "./imgs/Harry_Potter_character_poster.jpg",
+        
+    },
+    
+        turn_on: "Click",
+        play: "Play",
+        bannar_coming: "Banner",
+        balloons_flying: "Ballons",
+        cake_fadein: "Cake？",
+        light_candle: "Candle？",
+        wish_message: "Messages",
+        story: "A MESSAGE FOR YOU",
+    }
+
